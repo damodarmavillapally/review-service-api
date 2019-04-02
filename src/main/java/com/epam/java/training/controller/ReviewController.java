1e462f9eca07a06b6ea61319e33ba2778a7c5215
Damodar Mavillapally <Damodar_Mavillapally@epam.com>
2019-04-02 20:23:06 +0530

    Added Swagger implementation.

--- src/main/java/com/epam/java/training/controller/ReviewController.java
+++ src/main/java/com/epam/java/training/controller/ReviewController.java
@@ -17,12 +17,33 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.epam.java.training.model.Review;
 import com.epam.java.training.service.IReviewService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
+@Api(tags={"Review Service API"})
 public class ReviewController {
 	@Autowired
 	private IReviewService reviewService;
 
 	@RequestMapping(value = "/{productId}/reviews", method = RequestMethod.GET)
+	@ApiOperation(value="Product Reviews", notes="Provides the Product Review Information for a given productId", response=ResponseEntity.class)
+	@ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "Product Id", required=true, dataType="Integer", paramType="path"),
+        @ApiImplicitParam(name = "API_KEY", value = "api key", required=true, dataType="String", paramType="header")
+	})
+	@ApiResponses(value={
+			@ApiResponse(code=200, message="Success"),
+			@ApiResponse(code=401, message="Bad Request"),
+			@ApiResponse(code=402, message="Unauthorised"),
+			@ApiResponse(code=403, message="Forbidden"),
+			@ApiResponse(code=404, message="No Product is found"),
+			@ApiResponse(code=500, message="Internal Server Error")
+	})
 	public ResponseEntity<List<Review>> getProductReviews(@PathVariable("productId") int productId, @RequestHeader(name = "API_KEY", required = true) String headerApiKey) {
 		List<Review> listReviews =  null;
 		HttpHeaders headers = new HttpHeaders();
@@ -36,6 +57,20 @@ public class ReviewController {
 	}
 
 	@RequestMapping(value = "/{productId}/reviews", method = RequestMethod.POST)
+	@ApiOperation(value="Creates Product Review", notes="Creates the Product Review Information for a given productId", response=ResponseEntity.class)
+	@ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "Product Id", required=true, dataType="Integer", paramType="path"),
+        @ApiImplicitParam(name = "review", value = "review", required=true, dataType="Review", paramType="body"),
+        @ApiImplicitParam(name = "API_KEY", value = "api key", required=true, dataType="String", paramType="header")
+	})
+	@ApiResponses(value={
+			@ApiResponse(code=200, message="Success"),
+			@ApiResponse(code=401, message="Bad Request"),
+			@ApiResponse(code=402, message="Unauthorised"),
+			@ApiResponse(code=403, message="Forbidden"),
+			@ApiResponse(code=404, message="No Product is found"),
+			@ApiResponse(code=500, message="Internal Server Error")
+	})
 	public ResponseEntity<Void> createReviewProduct(@PathVariable("productId") int productId,
 			@RequestBody Review review, @RequestHeader(name = "API_KEY", required = true) String headerApiKey,
 			UriComponentsBuilder builder) {
@@ -60,6 +95,20 @@ public class ReviewController {
 	}
 
 	@RequestMapping(value = "/{productId}/reviews/{reviewId}", method = RequestMethod.PUT)
+	@ApiOperation(value="Updates Product Review", notes="Updates the Product Review Information for a given productId and reviewId", response=ResponseEntity.class)
+	@ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "Product Id", required=true, dataType="Integer", paramType="path"),
+        @ApiImplicitParam(name = "review id", value = "review id", required=true, dataType="Integer", paramType="path"),
+        @ApiImplicitParam(name = "API_KEY", value = "api key", required=true, dataType="String", paramType="header")
+	})
+	@ApiResponses(value={
+			@ApiResponse(code=200, message="Success"),
+			@ApiResponse(code=401, message="Bad Request"),
+			@ApiResponse(code=402, message="Unauthorised"),
+			@ApiResponse(code=403, message="Forbidden"),
+			@ApiResponse(code=404, message="No Product is found"),
+			@ApiResponse(code=500, message="Internal Server Error")
+	})
 	public ResponseEntity<Review> updateProductReview(@PathVariable("productId") int productId,
 			@PathVariable("reviewId") int reviewId, @RequestBody Review review,
 			@RequestHeader(name = "API_KEY", required = true) String headerApiKey) {
@@ -75,6 +124,20 @@ public class ReviewController {
 	}
 
 	@RequestMapping(value = "/{productId}/reviews/{reviewId}", method = RequestMethod.DELETE)
+	@ApiOperation(value="Deletes Product Review", notes="Deletes the Product Review Information for a given productId and reviewId", response=ResponseEntity.class)
+	@ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "Product Id", required=true, dataType="Integer", paramType="path"),
+        @ApiImplicitParam(name = "review id", value = "review id", required=true, dataType="Integer", paramType="path"),
+        @ApiImplicitParam(name = "API_KEY", value = "api key", required=true, dataType="String", paramType="header")
+	})
+	@ApiResponses(value={
+			@ApiResponse(code=200, message="Success"),
+			@ApiResponse(code=401, message="Bad Request"),
+			@ApiResponse(code=402, message="Unauthorised"),
+			@ApiResponse(code=403, message="Forbidden"),
+			@ApiResponse(code=404, message="No Product is found"),
+			@ApiResponse(code=500, message="Internal Server Error")
+	})
 	public ResponseEntity<Void> deleteProductReview(@PathVariable("productId") int productId,
 			@PathVariable("reviewId") int reviewId,
 			@RequestHeader(name = "API_KEY", required = true) String headerApiKey) {
