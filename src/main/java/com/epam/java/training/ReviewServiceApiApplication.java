f9c8c88ecba9721e4085f9e0c0c1e9c33b1e586f
Damodar Mavillapally <Damodar_Mavillapally@epam.com>
2019-04-16 17:41:42 +0530

    Code changes for Eureka Server Integration

--- src/main/java/com/epam/java/training/ReviewServiceApiApplication.java
+++ src/main/java/com/epam/java/training/ReviewServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.epam.java.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewServiceApiApplication {
 
 	public static void main(String[] args) {
